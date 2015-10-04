39de9e700a8c479146c736a13d65b9d9ab3706fa
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-10-04 17:05:14 +0100

    initial commit

---
+++ schwad.js
@@ -0,0 +1,59 @@
+//TODO ; NEED TO RESOLVE THE RENDERING ISSUE WHEN I COME BACK
+
+// 1. Set up global namespace object
+
+var MYAPP = {};
+MYAPP.myReturn = [];
+console.log(MYAPP.myReturn);
+MYAPP.mySubSpace = {};
+MYAPP.otherSubSpace = {};
+MYAPP.renderResult = function(input, index){
+ var myDiv = document.getElementById('my-display');
+ var newElement = document.createElement('p');
+    if (typeof input == "object"){
+        newElement.innerHTML = index + ". " + JSON.stringify(input);
+        myDiv.appendChild(newElement);
+        console.log(input)
+    } else {
+        newElement.innerHTML = index + ". " + input.toString();
+        myDiv.appendChild(newElement);
+    }
+}
+
+MYAPP.mySubSpace.myProp = "holla";
+
+MYAPP.myReturn.push(MYAPP.mySubSpace.myProp);
+
+// 2. Setting prototypes.
+var Country = {
+    sovereignty: true,
+    army: "oh yeah"
+}
+
+var america = {
+    population: 300000000,
+    president: "obama",
+}
+
+Object.setPrototypeOf(america, Country);
+MYAPP.myReturn.push(america.__proto__);
+//3. Other way to set proto
+
+var mexico = {
+    population: 85000000,
+    president: "Chavez"
+}
+
+mexico.__proto__ = Country;
+
+MYAPP.myReturn.push(mexico.__proto__);
+
+// functional chunk of code to render below; if it won't post gives warning and chunks to console log..
+
+for(i = 0; i < MYAPP.myReturn.length; i++){
+   MYAPP.renderResult(MYAPP.myReturn[i], i);
+}
+
+// execute
+//console.log(typeof //MYAPP.mySubSpace.myProp.__proto__);
+//MYAPP.renderResult(america.army);
